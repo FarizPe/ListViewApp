61d25340c8512c0c76b5cd34ed4ff34e299a2581
FarizPe <fariz.perdana212@gmail.com>
2021-10-22 14:15:00 +0700

    Aplikasi List View

---
+++ app/src/main/java/com/fariz/aplikasilistview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.fariz.aplikasilistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lvItem;
+    private String[] namanegara = new String[]{
+            "Indonesia","Malaysia","Singapure",
+            "Italia","Inggris","Belanda",
+            "Argentina","Chile",
+            "Mesir","Uganda"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("ListView Sederhana");
+
+        lvItem = (ListView) findViewById(R.id.list_view);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, namanegara);
+
+        lvItem.setAdapter(adapter);
+
+        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Memilih : "+namanegara[position], Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
